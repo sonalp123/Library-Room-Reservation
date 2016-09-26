232fa435947072f92df05b4db101dace37f73bcb
Sonal <sonalpatil@gmail.com>
2016-09-25 22:28:16 -0400

    Updated seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,9 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 User.create(username: "Superadmin", email: 'admin@lib.com', role: "admin", password: "admin", password_confirmation: "admin")
-
+LibraryRoom.create(number: 200, building: 'Hunt', size: 2)
+LibraryRoom.create(number: 300, building: 'Hunt', size: 4)
+LibraryRoom.create(number: 400, building: 'Hunt', size: 6)
+LibraryRoom.create(number: 250, building: 'DH Hill', size: 2)
+LibraryRoom.create(number: 350, building: 'DH Hill', size: 4)
+LibraryRoom.create(number: 450, building: 'DH Hill', size: 6)
