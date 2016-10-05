4c7621d786efb22b6412ad395cabcf16926795fe
Sonal <sonalpatil@gmail.com>
2016-10-04 21:56:17 -0400

    Minor revisions

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160925034516) do
+ActiveRecord::Schema.define(version: 20161005005249) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,9 +45,10 @@ ActiveRecord::Schema.define(version: 20160925034516) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "sender"
+    t.integer  "booking_id"
   end
 
-  create_table "users", primary_key: "username", force: :cascade do |t|
+  create_table "users", force: :cascade do |t|
     t.string   "username"
     t.string   "email"
     t.datetime "created_at",            null: false
@@ -59,5 +60,4 @@ ActiveRecord::Schema.define(version: 20160925034516) do
     t.string   "string"
   end
 
-  add_foreign_key "booking_histories", "users", column: "username", primary_key: "username", name: "booking_histories_username_fk"
 end
