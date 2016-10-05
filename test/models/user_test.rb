16650352fc23a8d8c168b62ebcd23b7be6e11475
Sonal <sonalpatil@gmail.com>
2016-10-05 13:37:04 -0400

    Test cases for user model and controller

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,6 +1,26 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
+  test "username should not be blank" do
+    user = User.new
+    assert_not user.save, "Created user without username"
+  end
+
+  test "password should not be blank" do
+    user = User.new
+    assert_not user.save, "Created user without password"
+  end
+
+  test "email should be in valid format" do
+    user = User.new
+    assert_match (user.email, /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i, "Valid email")
+  end
+
+  test "password_confirmation should not be blank" do
+    user = User.new
+    assert_not user.save, "Created user without password confirmation"
+  end
+
   # test "the truth" do
   #   assert true
   # end
