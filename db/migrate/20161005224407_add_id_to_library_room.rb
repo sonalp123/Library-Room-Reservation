503b81a77d23a84451c438cf8364a0b0a771dcfe
srjhawar <srjhawar@ncsu.edu>
2016-10-06 00:11:20 -0400

    Adding id key to library room

--- db/migrate/20161005224407_add_id_to_library_room.rb
+++
@@ -1,6 +0,0 @@
-class AddIdToLibraryRoom < ActiveRecord::Migration
-  def change
-    add_column :library_rooms ,:id, :integer
-    execute "ALTER TABLE library_rooms ADD PRIMARY KEY (id);"
-  end
-end
